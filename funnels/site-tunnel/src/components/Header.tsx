bf839a291e819a8b8b30bed6485f487621d586e3
Dany Gosselin <danygosselin68@hotmail.com>
2025-11-20 17:45:31 -0500

    backup 20 novembre 2025

--- funnels/site-tunnel/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="w-full bg-primary text-white p-4">
-      <div className="container mx-auto">
-        <h1 className="text-2xl font-bold">RabaisLocal</h1>
-      </div>
-    </header>
-  );
-}
