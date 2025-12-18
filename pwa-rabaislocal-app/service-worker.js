5425966a893ede784cba1721562fd7e5cb1f4f6b
Dany Gosselin <danygosselin68@hotmail.com>
2025-12-17 21:47:01 -0500

    Restructuration propre du projet : Suppression des dossiers test et production redundants

---
+++ pwa-rabaislocal-app/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "rabaislocal-app-prod-v13";
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png",
+    "./logo-rabaislocal.png",
+    "./splash-portrait.png"
+];
+
+self.addEventListener("install", (event) => {
+    // Install the new service worker immediately
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => Promise.all(
+            keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+        )).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cached) => {
+            return cached || fetch(event.request);
+        })
+    );
+});
